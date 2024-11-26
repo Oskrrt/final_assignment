095d804de8600d09f20bf20061b45989a9750e8d
Oskrrt <oskar_ruyter@hotmail.com>
2024-11-26 19:23:48 +0100

    Made initial file structure

---
+++ src/file_organizer/file_handler.py
@@ -0,0 +1,16 @@
+class FileHandler:
+
+    def __init__(self):
+        default_directory = ""
+
+    def move_file(self):
+        return ""
+
+    def create_directory(self):
+        return ""
+
+    def log_event(self, event):
+        return ""
+
+    def determine_file_type(self):
+        return ""

2ca789d76a64ba0a16647dc8b4f95369b38e5ff1
Oskrrt <oskar_ruyter@hotmail.com>
2024-12-04 07:26:38 +0100

    file_organizer almost finished

--- src/file_organizer/file_handler.py
+++ src/file_organizer/file_handler.py
@@ -1,101 +1,158 @@
 import tomllib
 import os
-import sys
-from itertools import filterfalse
 import shutil
 
-from file_organizer.logger import logged
+from file_organizer.logger import logged, log_error
 
 class FileHandler:
     CONFIG_FILE_PATH = os.path.join('src', 'file_organizer', 'file_config.toml')
-
-    def __init__(self):
-        print("cwd: " + os.getcwd())
-        self.default_directory = os.path.join('src', 'file_organizer', 'directories')
-
+    DEFAULT_DIRECTORY = os.path.join('src', 'file_organizer', 'directories')
+    TEXT_COLORS = {
+        "green": '\033[92m',
+        "orange": '\033[93m',
+        "red": '\033[91m',
+        "blue": '\033[94m'
+    }
+    
     # resets the files in the directory to the default structure, used for testing the organizer.
-    def reset_directory(self):
-        dirs = [d for d in os.listdir(self.default_directory) if os.path.isdir(os.path.join(self.default_directory, d))]
-        print(dirs)
-        for dir in dirs:
-            src_path = os.path.join(self.default_directory, dir)
-            print(src_path)
+    @classmethod
+    @logged("directory reset")
+    def reset_directory(cls):
+        directories = [d for d in os.listdir(cls.DEFAULT_DIRECTORY) if os.path.isdir(os.path.join(cls.DEFAULT_DIRECTORY, d))]
+        #print(directories)
+        for directory in directories:
+            src_path = os.path.join(cls.DEFAULT_DIRECTORY, directory)
             files_in_dir = os.listdir(src_path)
             for file in files_in_dir:
-                shutil.move(os.path.join(src_path, file), os.path.join(self.default_directory, file))
+                shutil.move(os.path.join(src_path, file), os.path.join(cls.DEFAULT_DIRECTORY, file))
             # was getting winerror 5, access denied on the directory deletion without ignore_errors=True
             shutil.rmtree(src_path, ignore_errors=True)
-
-        #shutil.rmtree(os.path.join(self.default_directory, dirs[0]), ignore_errors=True)
+        print(f"{cls.TEXT_COLORS["green"]}The directory has been reset")
+        #shutil.rmtree(os.path.join(cls.DEFAULT_DIRECTORY, dirs[0]), ignore_errors=True)
 
     # the main method in the class which does the file organizing.
-    def organize_files(self):
-        files = self.get_files()
-        print("Files before remove: " + str(files))
-        files = self.remove_unsupported_files(files)
-        print("Files after remove: " + str(files))
-        for file in files:
-            self.move_file(file)
-        return ""
+    @classmethod
+    def organize_files(cls):
+        files = cls.get_files()
+        if not files:
+            print(f"{cls.TEXT_COLORS["green"]}No files to organize")
+            return
+        files = cls.remove_unsupported_files(files)
+        try:
+            for file in files:
+                cls.move_file(file)
+            print(f"{cls.TEXT_COLORS["green"]}The directory has been organized")
+        except Exception as e:
+            log_error(e)
 
-    def remove_unsupported_files(self, files):
-        supported_file_types = self.get_supported_file_types_from_config()["filetypes"]
-        print("supported file types: " + str(supported_file_types))
+    # Removes any unsupported files.
+    @classmethod
+    def remove_unsupported_files(cls, files):
+        supported_file_types = cls.get_supported_file_types_from_config()["filetypes"]
         for file in files:
-            file_type = self.determine_file_type(file)
-            print(file_type)
+            file_type = cls.determine_file_type(file)
             if file_type not in supported_file_types:
-                #print("unsupported file type")
                 files.remove(file)
-                self.remove_file(file)
+                cls.remove_file(file)
         return files
 
-    #@logged("delete")
-    def remove_file(self, file):
-        path = os.path.join(self.default_directory, file)
+    @classmethod
+    @logged("delete")
+    def remove_file(cls, file):
+        path = os.path.join(cls.DEFAULT_DIRECTORY, file)
         if os.path.exists(path):
             os.remove(path)
-            print("removed file: " + path)
+            print(f"{cls.TEXT_COLORS["red"]}removed file with unsupported filetype: " + path)
         else:
             print("The file does not exist")
 
-    #@logged("file moved")
-    def move_file(self, file):
-        file_type = self.determine_file_type(file)
-        self.create_directory(file_type)
-        print("file: " + file)
-        src_path = os.path.join(self.default_directory, file)
-        print("srcPath: " + src_path)
-        dst_path = os.path.join(self.default_directory, file_type, file)
-        print("dstPath: " + dst_path)
-        shutil.move(src_path, dst_path)
+    # moves a file from the default directory into the correct folder based on filetype.
+    @classmethod
+    @logged("file moved")
+    def move_file(cls, file):
+        file_type = cls.determine_file_type(file)
+        src_path = os.path.join(cls.DEFAULT_DIRECTORY, file)
+        #print("srcPath: " + src_path)
+        dst_path = os.path.join(cls.DEFAULT_DIRECTORY, file_type, file)
+        #print("dstPath: " + dst_path)
+        new_dir_path = os.path.join(cls.DEFAULT_DIRECTORY, file_type)
+        try:
+            if not os.path.exists(new_dir_path):
+                cls.create_directory(new_dir_path)
+
+            shutil.move(src_path, dst_path)
+            print(f"{cls.TEXT_COLORS["orange"]}moved file: " + file + " from " + src_path + " to " + dst_path)
+        except Exception as e:
+            log_error(e)
+
+    @classmethod
+    @logged("directory created")
+    def create_directory(cls, path):
+        os.makedirs(path)
+        print(f"{cls.TEXT_COLORS["blue"]}created directory: " + path)
+
+    # Fetches all files in the default directory
+    @classmethod
+    def get_files(cls):
+        files = os.listdir(cls.DEFAULT_DIRECTORY)
+        # the below line makes sure the returned list only contains files and not directories
+        return [f for f in files if os.path.isfile(os.path.join(cls.DEFAULT_DIRECTORY, f))]
+
+    #@classmethod
+    #def test_error(cls):
+        #log_error(e)
+        #try:
+            #raise Exception("Dette er en excpetion")
+            #hei = "ba.le".split(".")
+            #print("hei[2]")
+        #except Exception as e:
+            #print("hei", file=sys.stderr)
+            #print(traceback.extract_stack())
+            #print(format_stack())
+            ##print(exc)
 
 
+    # @classmethod
+    # def log_error(cls, e):
+    #     function_name = inspect.currentframe().f_back.f_code.co_name
+    #     file_name = inspect.currentframe().f_code.co_filename
+    #     print(function_name)
+    #     code_block = '''
+    #     @logged("error")
+    #     def log_error_ast(e, function_name, file_name):
+    #         message = "Error: " + str(e) + " occurred in method: " + function_name + " in file: " + file_name
+    #         print(message, file=sys.stderr)
+    #
+    #     log_error_ast(e, function_name, file_name)
+    #     '''
+    #     code_block = textwrap.dedent(code_block)
+    #     parsed_code_block = ast.parse(code_block, mode='exec')
+    #     compiled_code = compile(parsed_code_block, '<string>', 'exec')
+    #     # Execute the compiled code
+    #     exec(compiled_code)
 
 
-    #@logged("directory created")
-    def create_directory(self, dir_name):
-        path = os.path.join(self.default_directory, dir_name)
-        print("dir path: " + path)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        return ""
+    #@classmethod
+    # @logged("error")
+    # def log_error_2(cls, e):
+    #     function_name = inspect.currentframe().f_back.f_back.f_code.co_name
+    #     file_name = inspect.currentframe().f_code.co_filename
+    #     message = "Error: " + repr(e) + " occurred in method: " + function_name + " in file: " + file_name
+    #     print(message, file=sys.stderr)
+    #     #raise e
 
-    def get_files(self):
-        #return os.listdir(self.default_directory)
-        files = os.listdir(self.default_directory)
-        return [f for f in files if os.path.isfile(os.path.join(self.default_directory, f))]
-
-    def log_event(self, event):
-        return ""
-
-    def determine_file_type(self, file):
+    # Determines the filetype based on what comes after "."
+    @classmethod
+    def determine_file_type(cls, file):
         file_type = file.split(".")[1]
-        #print(file_type)
         return file_type
 
-    #@logged("error")
-    def get_supported_file_types_from_config(self):
-        with open(self.CONFIG_FILE_PATH, "rb") as f:
-            #print(tomllib.load(f))
-            return tomllib.load(f)["filetypes"]
+    # Fetches all supported file types configured in the file_config.toml file
+    @classmethod
+    def get_supported_file_types_from_config(cls):
+        try:
+            with open(cls.CONFIG_FILE_PATH, "rb") as f:
+                return tomllib.load(f)["filetypes"]
+        except Exception as e:
+            log_error(e)
+
--- src/file_organizer/logger.py
+++ src/file_organizer/logger.py
@@ -1,24 +1,55 @@
+import inspect
 import os
 import datetime
+import traceback
 from functools import wraps
 
+#LOG_FILE_PATH = os.path.join('src', 'file_organizer', 'logs', 'file_handling_logs.txt')
 
-
-
-def logged(log_event, file=None, message=None):
+def logged(log_event):
     def log(func):
-        if log_event == "error":
-            log_file_path = os.path.join('src', 'file_organizer', 'logs', 'error_logs.txt')
-        else:
-            log_file_path = os.path.join('src', 'file_organizer', 'logs', 'file_handling_logs.txt')
-        # logs the message sending event to resources/message_logs.txt
+        # logs the file handling events to src/file_organizer/logs/file_handling_logs.txt
+        # and the error events to src/file_organizer/logs/error_logs.txt
         @wraps(func)
         def wrapper(*args, **kwargs):
+            if log_event == "error":
+                log_file_path = os.path.join('src', 'file_organizer', 'logs', 'error_logs.txt')
+            else:
+                log_file_path = os.path.join('src', 'file_organizer', 'logs', 'file_handling_logs.txt')
+            # generate message content based on log_event
+            message_to_log = create_log_message(log_event, args)
             try:
                 with open(log_file_path, "a") as log_file:
-                    log_file.write(f"{datetime.datetime.now()} - {log_event}: on file {file} {message}\n")
+                    log_file.write(message_to_log)
+                    return func(*args, **kwargs)
             except FileNotFoundError as e:
                 print("File not found: " + e.filename)
                 raise e
         return wrapper
     return log
+
+def create_log_message(log_event, message_content):
+    message_to_log = ""
+    if log_event == "directory reset":
+        message_to_log = f"{datetime.datetime.now()} - {log_event} to original state, deleted subfolders\n"
+    elif log_event == "delete":
+        message_to_log = f"{datetime.datetime.now()} - {log_event} file: {message_content[1]}\n"
+    elif log_event == "file moved":
+        message_to_log = f"{datetime.datetime.now()} - {log_event} to correct directory - file: {message_content[1]}\n"
+    elif log_event == "directory created":
+        message_to_log = f"{datetime.datetime.now()} - {log_event}: {message_content[1]}\n"
+    elif log_event == "error":
+        exc = traceback.format_exc()
+        message_to_log = f"{datetime.datetime.now()} - {log_event}: {exc}\n"
+
+    return message_to_log
+
+# Handles errors and shuts down the execution of the program.
+@logged("error")
+def log_error(e, message=None):
+    function_name = inspect.currentframe().f_back.f_back.f_code.co_name
+    file_name = inspect.currentframe().f_back.f_back.f_code.co_filename
+    message = "Error: " + repr(e) + " occurred in method: " + function_name + " in file: " + file_name
+    red = '\033[91m'
+    print(f"{red}" + message)
+    #exit(1)
